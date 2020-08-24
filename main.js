a4214c10ebd6c080d88395a1fb7dab3d38869d40
Ritesh <riteshkumarstudy@gmail.com>
2020-08-24 17:58:43 +0530

    JQUERY added. Responsive CSS added.

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
     $('.menu-toggler').on('click', function() {
-        $(this).toggleClass('open');
+        $('.menu-toggler').toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
-});
+
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    /* This part needs correction. Scrolling effect not working */
+    $('nav a[href*="#"]').on('click', function(){
+        $('html, body').animate( keyframes ,{
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, options,4000);
+    });
+
+    $('#up').on('click',function(){
+        $('html, body').animate( keyframes, {
+            scrollTop: 0
+        }, options, 2000)
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1000,
+        once: true
+    });
+});    
